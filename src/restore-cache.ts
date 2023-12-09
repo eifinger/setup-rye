5934e348ec48ba5483e424109b69cf3a27d1e7b7
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2023-12-09 17:34:22 +0800

    add working-directory parameter (#113)

--- src/restore-cache.ts
+++ src/restore-cache.ts
@@ -6,8 +6,10 @@ import {getLinuxInfo} from './utils'
 
 export const STATE_CACHE_PRIMARY_KEY = 'cache-primary-key'
 export const CACHE_MATCHED_KEY = 'cache-matched-key'
-const CACHE_DEPENDENCY_PATH = '**/requirements**.lock'
-const cachePath = `${process.env['GITHUB_WORKSPACE']}/.venv`
+const CACHE_DEPENDENCY_PATH = 'requirements**.lock'
+const workingDir = `/${core.getInput('working-directory')}` || ''
+const cachePath = `${process.env['GITHUB_WORKSPACE']}${workingDir}/.venv`
+const cacheDependencyPath = `${process.env['GITHUB_WORKSPACE']}${workingDir}/${CACHE_DEPENDENCY_PATH}`
 
 export async function restoreCache(
   cachePrefix: string,
@@ -16,7 +18,7 @@ export async function restoreCache(
   const {primaryKey, restoreKey} = await computeKeys(cachePrefix, version)
   if (primaryKey.endsWith('-')) {
     throw new Error(
-      `No file in ${process.cwd()} matched to [${CACHE_DEPENDENCY_PATH}], make sure you have checked out the target repository`
+      `No file in ${process.cwd()} matched to [${cacheDependencyPath}], make sure you have checked out the target repository`
     )
   }
 
@@ -39,12 +41,13 @@ async function computeKeys(
   cachePrefix: string,
   version: string
 ): Promise<{primaryKey: string; restoreKey: string}> {
-  const hash = await glob.hashFiles(CACHE_DEPENDENCY_PATH)
+  core.debug(`Computing cache key for ${cacheDependencyPath}`)
+  const hash = await glob.hashFiles(cacheDependencyPath)
   let primaryKey = ''
   let restoreKey = ''
   const osInfo = await getLinuxInfo()
-  primaryKey = `${cachePrefix}-${process.env['RUNNER_OS']}-${osInfo.osVersion}-${osInfo.osName}-rye-${version}-venv-${hash}`
-  restoreKey = `${cachePrefix}-${process.env['RUNNER_OS']}-${osInfo.osVersion}-${osInfo.osName}-rye-${version}-venv`
+  primaryKey = `${cachePrefix}-${process.env['RUNNER_OS']}-${osInfo.osVersion}-${osInfo.osName}-rye-${version}-${workingDir}-${hash}`
+  restoreKey = `${cachePrefix}-${process.env['RUNNER_OS']}-${osInfo.osVersion}-${osInfo.osName}-rye-${version}-${workingDir}`
   return {primaryKey, restoreKey}
 }
 
